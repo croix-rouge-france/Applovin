12b1422fff3ac9fe789d320478189dad8b5ad24d
croix-rouge-france <adviserfinancial54@gmail.com>
2025-05-16 13:49:07 +0200

    Update config.inc.php

--- phpmyadmin/config.inc.php
+++ phpmyadmin/config.inc.php
@@ -1,13 +1,14 @@
 <?php
-$cfg['blowfish_secret'] = 'votre_secret_32_caracteres'; // Générer une clé aléatoire (32 caractères)
+$cfg['blowfish_secret'] = 'x7k9m2p8q5w3e6r4t2y1u0i9o7a5s3d'; // Exemple : 'x7k9m2p8q5w3e6r4t2y1u0i9o7a5s3d'
 $i = 0;
 $i++;
 $cfg['Servers'][$i]['auth_type'] = 'config';
-$cfg['Servers'][$i]['host'] = 'mysql-18eb1c5a-adviserfinancial54-80f2.l.aivencloud.com'; // Remplacer par votre hôte Aiven
-$cfg['Servers'][$i]['port'] = '18179'; // Remplacer par votre port Aiven
-$cfg['Servers'][$i]['user'] = 'avnadmin'; // Remplacer par votre utilisateur Aiven
-$cfg['Servers'][$i]['password'] = 'AVNS_CGN0rjVzbogydSu2O4O'; // Remplacer par votre mot de passe Aiven
-$cfg['Servers'][$i]['ssl'] = true; // Aiven exige SSL
+$cfg['Servers'][$i]['host'] = 'mysql-18eb1c5a-adviserfinancial54-80f2.l.aivencloud.com';
+$cfg['Servers'][$i]['port'] = '18179';
+$cfg['Servers'][$i]['user'] = 'avnadmin';
+$cfg['Servers'][$i]['password'] = 'AVNS_CGN0rjVzbogydSu2O4O';
+$cfg['Servers'][$i]['ssl'] = true;
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
+$cfg['Servers'][$i]['pmadb'] = 'defaultdb'; // Ajout pour la base de gestion de phpMyAdmin (optionnel)
 ?>
