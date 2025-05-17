a473c821d979e654af40d5de86e4d5a444c5ab0f
croix-rouge-france <adviserfinancial54@gmail.com>
2025-05-17 17:13:10 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require_once 'includes/config.php';
 require_once 'includes/db.php';
 require_once 'includes/auth.php';
-require_once 'includes/functions.php;
+
 
 
 if (isset($_SESSION['user_id'])) {
