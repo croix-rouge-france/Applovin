3551652548a9b22600eeb52bc08f437fa1d9fc2b
croix-rouge-france <adviserfinancial54@gmail.com>
2025-05-17 18:07:58 +0200

    Update deposit.php

--- deposit.php
+++ deposit.php
@@ -4,8 +4,8 @@ require_once 'includes/config.php';
 require_once 'includes/db.php';
 
 // Inclure manuellement les fichiers PayDunya (ajustez le chemin selon l'emplacement réel)
-require_once __DIR__ . '/lib/paydunya-php-master/paydunya/Setup.php';
-require_once __DIR__ . '/lib/paydunya-php-master/paydunya/checkout/Store.php';
+require_once __DIR__ . '/lib/paydunya-php-master/paydunya/setup.php';
+require_once __DIR__ . '/lib/paydunya-php-master/paydunya/checkout/store.php';
 require_once __DIR__ . '/lib/paydunya-php-master/paydunya/checkout/checkout_invoice.php';
 
 // Activation du mode debug
