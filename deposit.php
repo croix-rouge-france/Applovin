e338fa23037ff20ef4d5483044f051232874a311
croix-rouge-france <adviserfinancial54@gmail.com>
2025-05-19 15:55:43 +0200

    Update deposit.php

--- deposit.php
+++ deposit.php
@@ -14,6 +14,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
+error_reporting(E_ALL & ~E_DEPRECATED);
 
 // Vérification si l'utilisateur est connecté
 if (!isset($_SESSION['user_id'])) {
