095789f0b10b55833dda98d6c0a9963d14aa3c4e
croix-rouge-france <adviserfinancial54@gmail.com>
2025-05-19 19:14:44 +0200

    Update deposit.php

--- deposit.php
+++ deposit.php
@@ -74,7 +74,7 @@ $invoice = new CheckoutInvoice();
 $invoice->addItem("Plan Investissement $plan_id", 1, $xof_amount, $xof_amount, "Dépôt pour Applovin Plan $plan_id");
 $invoice->setTotalAmount($xof_amount);
 $invoice->setDescription("Paiement pour plan d'investissement $plan_id");
-$invoice->addChannels(['orange-money-senegal', 'wave-senegal', 'free-money']); // Canaux Mobile Money
+$invoice->addChannels(['orange-money-senegal', 'wave-senegal']); // Canaux Mobile Money
 
 // Ajouter des données personnalisées
 $invoice->addCustomData('user_id', $_SESSION['user_id']);
